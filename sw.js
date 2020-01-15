af637a9650b7139e64632a202c7ab18c3700ed8c
Vineet <vineetsethi1998@gmail.com>
2020-01-15 12:10:30 +0530

    debuging

--- sw.js
+++ sw.js
@@ -23,32 +23,43 @@
 //    e.waitUntil(self.registration.showNotification("Hello world!", options));
 // });
 
+self.addEventListener("push", function(e) {
+   console.log("in push fn in sw");
+   var body;
 
-self.addEventListener('push', function(e) {
-    var body;
-  
-    if (e.data) {
+   if (e.data) {
       body = e.data.text();
-    } else {
-      body = 'Push message no payload';
-    }
-  
-    var options = {
+   } else {
+      body = "Push message no payload";
+   }
+
+   console.log("Extracted body : " + body);
+
+   var options = {
       body: body,
-      icon: 'images/notification-flat.png',
+      icon: "images/notification-flat.png",
       vibrate: [100, 50, 100],
       data: {
-        dateOfArrival: Date.now(),
-        primaryKey: 1
+         dateOfArrival: Date.now(),
+         primaryKey: 1
       },
       actions: [
-        {action: 'explore', title: 'Explore this new world',
-          icon: 'images/checkmark.png'},
-        {action: 'close', title: 'I don\'t want any of this',
-          icon: 'images/xmark.png'},
+         {
+            action: "explore",
+            title: "Explore this new world",
+            icon: "images/checkmark.png"
+         },
+         {
+            action: "close",
+            title: "I don't want any of this",
+            icon: "images/xmark.png"
+         }
       ]
-    };
-    e.waitUntil(
-      self.registration.showNotification('Push Notification', options)
-    );
-  });
+   };
+   console.log("after setting options");
+
+   e.waitUntil(
+      self.registration.showNotification("Push Notification", options)
+   );
+   console.log("sent notification");
+});
