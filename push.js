0e9992d50ef5474fd33bcf5f015c9252bce4f13c
Vineet <vineetsethi1998@gmail.com>
2020-01-15 11:23:09 +0530

    changes in sw

--- push.js
+++ push.js
@@ -1,18 +1,34 @@
 //on server side
 
-var push = require('web-push');
+var push = require("web-push");
 
 //let vapidKeys = push.generateVAPIDKeys();
 
 //console.log(vapidKeys);
 // hardcode the values instead on generating them each time
 
-let vapidKeys =
-{ publicKey:
-    'BEupLAzaluKb5-uGeZbhN7H9UPMaRHk85IHD_qXZvTpBWvQZeo8Lo0Lk1cekr2iwNsl-Tdql2HyN8biVgswAJwU', //goes out with the web app - to be addeed in the appln server key in subscribe
-   privateKey: '2KVhIoQy-odo_iitxbjiNDqTwJhrMx6-QHZ41WgpChQ' }
+let vapidKeys = {
+   publicKey:
+      "BEupLAzaluKb5-uGeZbhN7H9UPMaRHk85IHD_qXZvTpBWvQZeo8Lo0Lk1cekr2iwNsl-Tdql2HyN8biVgswAJwU", //goes out with the web app - to be addeed in the appln server key in subscribe
+   privateKey: "2KVhIoQy-odo_iitxbjiNDqTwJhrMx6-QHZ41WgpChQ"
+};
 
-push.setVapidDetails('mailto:vksethi_b16@ce.vjti.ac.in', vapidKeys.publicKey, vapidKeys.privateKey);
+push.setVapidDetails(
+   "mailto:vksethi_b16@ce.vjti.ac.in",
+   vapidKeys.publicKey,
+   vapidKeys.privateKey
+);
 
 //pull sub out of the db - stored ppl that want to recieve notifications (value of JSON.stringify(push))
-push.sendNotification(sub, 'test message')
+let sub = {
+   endpoint:
+      "https://fcm.googleapis.com/fcm/send/dx8UE9hZNKM:APA91bFxfmcwKyL5Vev90cLll4_ZVlUqO6kRyavieKzHsnwX1yeAew122nWX3chhkZVsGsf8IjemogueleuFzoEfuWU7wxPtukWVY7417lAfKIZtv_AemuR-rtSiyaTRfAuXE5OZO0jZ",
+   expirationTime: null,
+   keys: {
+      p256dh:
+         "BH6luXQNTv7dOfGyXIzZOAk3AeIJwnr5YJazJtPcBLXxQ8bb085bwn8k5_cqCy76HqFetXlaXrGKQNUrmubeJn0",
+      auth: "dUHNLAmziajgsRIEMM0_6Q"
+   }
+};
+
+push.sendNotification(sub, "test message");
